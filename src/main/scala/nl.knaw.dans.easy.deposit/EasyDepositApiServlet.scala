a59b40975d4b6b5d71e70874f8c1c60afc647376
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2018-03-08 14:40:20 +0100

    Processed some review comments. Made a start with the login API.

--- src/main/scala/nl.knaw.dans.easy.deposit/EasyDepositApiServlet.scala
+++ src/main/scala/nl.knaw.dans.easy.deposit/EasyDepositApiServlet.scala
@@ -24,6 +24,6 @@ class EasyDepositApiServlet(app: EasyDepositApiApp) extends ScalatraServlet with
 
   get("/") {
     contentType = "text/plain"
-    Ok("EASY Deposit API Service running...")
+    Ok(s"EASY Deposit API Service running (${app.getVersion}")
   }
 }
