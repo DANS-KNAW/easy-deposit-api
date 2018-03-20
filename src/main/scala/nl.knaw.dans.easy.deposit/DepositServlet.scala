549c5d7b52ac7537ded7126cf3292ecae78dc0be
Joke Pol <joke.pol@dans.knaw.nl>
2018-03-20 11:55:08 +0100

    review comments

--- src/main/scala/nl.knaw.dans.easy.deposit/DepositServlet.scala
+++ src/main/scala/nl.knaw.dans.easy.deposit/DepositServlet.scala
@@ -48,15 +48,16 @@ class DepositServlet(app: EasyDepositApiApp) extends ScalatraServlet with DebugE
 
   private def forId[T](callback: (String, UUID) => Try[T]): ActionResult = {
     getUUID match {
-      case Failure(t) => badId(t)
+      case Failure(t) => BadRequest(badId(t))
       case Success(uuid) => respond(callback(userId, uuid))
     }
   }
 
   private def forPath[Result](callback: (String, UUID, Path) => Try[Result]): ActionResult = {
     (getUUID, getPath) match {
-      case (Failure(t), _) => badId(t)
-      case (_, Failure(t)) => BadRequest(s"Invalid path: ${ t.getClass.getName } ${ t.getMessage }")
+      case (Failure(tId), Failure(tPath)) => BadRequest(s"${ badId(tId) }. ${ badPath(tPath) }.")
+      case (Failure(tId), _) => BadRequest(badId(tId))
+      case (_, Failure(t)) => BadRequest(badPath(t))
       case (Success(uuid), Success(path)) => respond(callback(userId, uuid, path))
     }
   }
@@ -64,8 +65,14 @@ class DepositServlet(app: EasyDepositApiApp) extends ScalatraServlet with DebugE
   private def respond[Result](result: Try[Result]): ActionResult = {
     result match {
       case Success(Unit) => Ok()
-      case Success(None) => Ok()
-      case Success(_) => ???
+      case Success(seq: Seq[_]) if seq.isInstanceOf[DepositInfo] => Ok(???)
+      case Success(datasetMetadata: DatasetMetadata) => Ok(???)
+      case Success(stateInfo: StateInfo) => Ok(???)
+      case Success(result: Boolean) => Ok(result.toString) // writeDepositFile
+      case Success(uuid: UUID) => Ok(uuid.toString)
+      case Success(x) =>
+        logger.error(s"not expected result type: ${ x.getClass.getName }")
+        InternalServerError("not expected exception")
       // TODO case Failure(t: ???) =>
       case Failure(t) =>
         logger.error(t.getMessage, t)
@@ -73,34 +80,38 @@ class DepositServlet(app: EasyDepositApiApp) extends ScalatraServlet with DebugE
     }
   }
 
-  private def getUUID = Try {
+  private def getUUID: Try[UUID] = Try {
     UUID.fromString(params("uuid"))
   }
 
-  private def getPath = Try {
+  private def getPath: Try[Path] = Try {
     Paths.get(multiParams("splat").find(!_.trim.isEmpty).getOrElse(""))
   }
 
-  private def badId(t: Throwable) = {
-    BadRequest(s"Invalid deposit id: ${ t.getClass.getName } ${ t.getMessage }")
+  private def badId(t: Throwable): String = {
+    s"Invalid deposit id: ${ t.getClass.getName } ${ t.getMessage }"
+  }
+
+  private def badPath[Result](t: Throwable) = {
+    s"Invalid path: ${ t.getClass.getName } ${ t.getMessage }"
   }
 
   private def getInputStream: Try[InputStream] = ???
 
   private implicit val jsonFormats: Formats = new DefaultFormats {}
 
-  private def getStateInfo = Try {
+  private def getStateInfo: Try[StateInfo] = Try {
     // TODO verify mime type?
     JsonMethods.parse(request.body).extract[StateInfo]
     // TODO error recovery into something understandable
   }
 
-  private def getDatasetMetadata = Try {
+  private def getDatasetMetadata: Try[DatasetMetadata] = Try {
     // TODO as getStateInfo
     JsonMethods.parse(request.body).extract[DatasetMetadata]
   }
 
-  private def badBody(t: Throwable) = {
+  private def badBody(t: Throwable): ActionResult = {
     BadRequest(s"${ t.getClass.getName } ${ t.getMessage }")
   }
 }
