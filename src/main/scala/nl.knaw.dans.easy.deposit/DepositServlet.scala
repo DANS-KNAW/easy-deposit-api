a383c863015045ac7e0722835b3ef8a5724f259d
Joke Pol <joke.pol@dans.knaw.nl>
2018-03-27 13:21:31 +0200

    json methods to companion object

--- src/main/scala/nl.knaw.dans.easy.deposit/DepositServlet.scala
+++ src/main/scala/nl.knaw.dans.easy.deposit/DepositServlet.scala
@@ -23,10 +23,11 @@ import java.util.UUID
 import nl.knaw.dans.easy.deposit.DepositServlet._
 import nl.knaw.dans.easy.deposit.authentication.ServletEnhancedLogging._
 import nl.knaw.dans.lib.error._
-import org.json4s.ext.{ EnumNameSerializer, JodaTimeSerializers }
+import org.json4s.JsonAST.{ JNull, JString }
+import org.json4s.ext.{ EnumNameSerializer, JodaTimeSerializers, UUIDSerializer }
 import org.json4s.native.JsonMethods
 import org.json4s.native.Serialization.write
-import org.json4s.{ DefaultFormats, Formats }
+import org.json4s.{ CustomSerializer, DefaultFormats, Formats, JsonInput }
 import org.scalatra._
 
 import scala.util.{ Failure, Try }
@@ -54,76 +55,79 @@ class DepositServlet(app: EasyDepositApiApp) extends AbstractAuthServlet(app) {
         ))
     )
   }
-  get("/:id/metadata") {
+  get("/:uuid/metadata") {
     recoverIfFailureAndLog(
       forDeposit(app.getDatasetMetadataForDeposit)
         .map(datasetMetadata => Ok(body = toJson(datasetMetadata)))
     )
   }
-  put("/:id/metadata") {
+  put("/:uuid/metadata") {
     recoverIfFailureAndLog(for {
-      datasetMetadata <- getDatasetMetadata
+      datasetMetadata <- getDatasetMetadata(request.body)
       _ <- forDeposit(app.writeDataMetadataToDeposit(datasetMetadata))
     } yield Ok(???))
   }
-  get("/:id/state") {
+  get("/:uuid/state") {
     recoverIfFailureAndLog(
       forDeposit(app.getDepositState)
         .map(depositState => Ok(body = toJson(depositState)))
     )
   }
-  put("/:id/state") {
+  put("/:uuid/state") {
     recoverIfFailureAndLog(for {
-      stateInfo <- getStateInfo
+      stateInfo <- getStateInfo(request.body)
       _ <- forDeposit(app.setDepositState(stateInfo))
     } yield Ok(???))
   }
-  delete("/:id") {
+  delete("/:uuid") {
     recoverIfFailureAndLog(
       forDeposit(app.deleteDeposit)
         .map(_ => Ok(???))
     )
   }
-  get("/:id/file/*") { //dir and file
+  get("/:uuid/file/*") { //dir and file
     recoverIfFailureAndLog(
       forPath(app.getDepositFiles)
         .map(depositFiles => Ok(body = toJson(depositFiles)))
     )
   }
-  post("/:id/file/*") { //dir
+  post("/:uuid/file/*") { //dir
     recoverIfFailureAndLog(for {
       inputStream <- getInputStream
       newFileWasCreated <- forPath(app.writeDepositFile(inputStream))
     } yield Ok(???))
   }
-  put("/:id/file/*") { //file
+  put("/:uuid/file/*") { //file
     recoverIfFailureAndLog(for {
       inputStream <- getInputStream
       newFileWasCreated <- forPath(app.writeDepositFile(inputStream))
     } yield Ok(???))
 
   }
-  delete("/:id/file/*") { //dir and file
+  delete("/:uuid/file/*") { //dir and file
     recoverIfFailureAndLog(
       forPath(app.deleteDepositFile)
         .map(_ => Ok(???))
     )
   }
 
-  private def forUser[T](callback: (String) => Try[T]): Try[T] = {
-    // shortest callBack signature of the forXxx(callBack) methods
+  private def forUser[T](callback: (String) => Try[T]
+                        ): Try[T] = {
+    // shortest callBack signature
     // the signatures reflect the parameters in the route patterns and authenticated user
     Try(callback(user.id)).flatten // catch throws that slipped through
   }
 
-  private def forDeposit[T](callback: (String, UUID) => Try[T]): Try[T] = {
+  private def forDeposit[T](callback: (String, UUID) => Try[T]
+                           ): Try[T] = {
     for {
       uuid <- getUUID
       result <- Try(callback(user.id, uuid)).flatten // catch throws that slipped through
     } yield result
   }
 
-  private def forPath[Result](callback: (String, UUID, Path) => Try[Result]): Try[Result] = {
+  private def forPath[T](callback: (String, UUID, Path) => Try[T]
+                        ): Try[T] = {
     for {
       uuid <- getUUID
       path <- getPath
@@ -131,13 +135,15 @@ class DepositServlet(app: EasyDepositApiApp) extends AbstractAuthServlet(app) {
     } yield result
   }
 
-
   private def recoverIfFailureAndLog(appResult: Try[ActionResult]): ActionResult = {
     appResult.getOrRecover {
       // TODO case Failure(t: ???) =>
       case t: InvalidResource =>
         logger.error(s"InvalidResource: ${ t.getMessage }")
         NotFound()
+      case t: InvalidDocument =>
+        logger.error(s"Invalid ${ t.getMessage }:${ t.getCause.getClass.getName } ${ t.getCause.getMessage }")
+        BadRequest(s"Bad Request. The ${ t.getMessage } document is malformed.")
       case t =>
         logger.error(s"Not expected exception: ${ t.getMessage }", t)
         InternalServerError("Internal Server Error")
@@ -158,35 +164,42 @@ class DepositServlet(app: EasyDepositApiApp) extends AbstractAuthServlet(app) {
   }
 
   private def getInputStream: Try[InputStream] = ???
+}
+
+object DepositServlet {
+
+  private class InvalidResource(s: String) extends Exception(s)
+  private class InvalidDocument(s: String, t: Throwable) extends Exception(s, t)
+
+  class PathSerializer extends CustomSerializer[Path](format =>
+    ( {
+      case JString(s) => Paths.get(s)
+      case JNull => null
+    }, {
+      case x: Path => JString(x.toString)
+    }
+    )
+  )
 
   private implicit val jsonFormats: Formats = new DefaultFormats {
+    // TODO we need a timestamp for DepositInfo, but may need dates for not yet implemented members of DatasetMetadata
     override protected def dateFormatter: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
-  } + new EnumNameSerializer(State) ++ JodaTimeSerializers.all
+  } +
+    UUIDSerializer +
+    new PathSerializer +
+    new EnumNameSerializer(State) ++
+    JodaTimeSerializers.all
 
-
-  def toJson[A <: AnyRef](a: A): String = {
-    // just for readability
+  private def toJson[A <: AnyRef](a: A): String = {
     // seems not to need a try: while the date formatter wasn't in place it produced empty strings
     write(a)
   }
 
-  private def getStateInfo: Try[StateInfo] = Try {
-    // TODO verify mime type?
-    JsonMethods.parse(request.body).extract[StateInfo]
-  }.recoverWith { case t: Throwable =>
-    logger.error(s"bad StateInfo:${ t.getClass.getName } ${ t.getMessage }")
-    Failure(new Exception(s"Bad Request. The state document is malformed."))
-  }
+  private def getStateInfo(body: JsonInput): Try[StateInfo] = Try {
+    JsonMethods.parse(body).extract[StateInfo]
+  }.recoverWith { case t: Throwable => Failure(new InvalidDocument("StateInfo", t)) }
 
-  private def getDatasetMetadata: Try[DatasetMetadata] = Try {
-    // TODO verify mime type?
-    JsonMethods.parse(request.body).extract[DatasetMetadata]
-  }.recoverWith { case t: Throwable =>
-    logger.error(s"bad DatasetMetadata:${ t.getClass.getName } ${ t.getMessage }")
-    Failure(new Exception(s"Bad Request. The metadata document is malformed."))
-  }
-}
-object DepositServlet {
-
-  private class InvalidResource(s: String) extends Exception(s)
+  private def getDatasetMetadata(body: JsonInput): Try[DatasetMetadata] = Try {
+    JsonMethods.parse(body).extract[DatasetMetadata]
+  }.recoverWith { case t: Throwable => Failure(new InvalidDocument("DatasetMetadata", t)) }
 }
--- src/test/scala/nl.knaw.dans.easy.deposit/HappyRoutesSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.deposit/HappyRoutesSpec.scala
@@ -15,11 +15,14 @@
  */
 package nl.knaw.dans.easy.deposit
 
+import java.nio.file.{ Path, Paths }
 import java.util.UUID
 
+import nl.knaw.dans.easy.deposit.State._
 import nl.knaw.dans.easy.deposit.authentication.AuthenticationMocker._
 import nl.knaw.dans.easy.deposit.authentication.AuthenticationProvider
 import org.eclipse.jetty.http.HttpStatus._
+import org.joda.time.DateTime
 import org.scalamock.scalatest.MockFactory
 import org.scalatra.test.scalatest.ScalatraSuite
 
@@ -40,7 +43,7 @@ class HappyRoutesSpec extends TestSupportFixture with ServletFixture with Scalat
   addServlet(new EasyDepositApiServlet(mockedApp), "/*")
 
   "get /" should "be ok" in {
-    mockedApp.getVersion _ expects () returning "test"
+    mockedApp.getVersion _ expects() returning "test"
     expectsNoUser
     get(uri = "/") {
       body shouldBe "EASY Deposit API Service running (test)"
@@ -72,6 +75,57 @@ class HappyRoutesSpec extends TestSupportFixture with ServletFixture with Scalat
       status shouldBe OK_200
       body shouldBe s"$uuid"
       header("Location") should (fullyMatch regex s"http://localhost:[0-9]+/deposit/$uuid")
+    }
+  }
+
+  "get /deposit" should "return DepositInfo records" in {
+    val uuid1 = UUID.randomUUID()
+    val uuid2 = UUID.randomUUID()
+    expectsUserFooBar
+    (mockedApp.getDeposits(_: String)) expects "foo" returning Success(Seq(
+      DepositInfo(uuid1, "x", DRAFT, "a", new DateTime("2018-03-27")),
+      DepositInfo(uuid2, "y", SUBMITTED, "b", new DateTime("2018-03-22"))
+    ))
+
+    get(
+      uri = "/deposit",
+      headers = Seq(("Authorization", fooBarBasicAuthHeader))
+    ) {
+      status shouldBe OK_200
+      val info1 = s"""{"id":"$uuid1","title":"x","state":"DRAFT","stateDescription":"a","timestamp":"2018-03-27"}"""
+      val info2 = s"""{"id":"$uuid2","title":"y","state":"SUBMITTED","stateDescription":"b","timestamp":"2018-03-22"}"""
+      body shouldBe s"""[$info1,$info2]"""
+    }
+  }
+
+  private val fixedUUID = UUID.fromString("1cd9409d-8645-46a0-80db-eaf468a5ba7e")
+
+  s"get /$fixedUUID/state" should "return DatasetMetadata" in {
+    expectsUserFooBar
+    // TODO how to define expects for the curried method called by the PUT variant?
+    (mockedApp.getDepositState(_: String, _: UUID)) expects("foo", fixedUUID) returning
+      Success(StateInfo(DRAFT, "x"))
+
+    get(
+      uri = s"/deposit/$fixedUUID/state",
+      headers = Seq(("Authorization", fooBarBasicAuthHeader))
+    ) {
+      body shouldBe s"""{"state":"DRAFT","stateDescription":"x"}"""
+      status shouldBe OK_200
+    }
+  }
+
+  s"get /$fixedUUID/file/a.txt" should "return FileInfo" in {
+    expectsUserFooBar
+    (mockedApp.getDepositFiles(_: String, _: UUID, _: Path)) expects("foo", fixedUUID, *) returning
+      Success(Seq(FileInfo("a.txt", Paths.get("files/a.txt"), "x")))
+
+    get(
+      uri = s"/deposit/$fixedUUID/file/a.txt",
+      headers = Seq(("Authorization", fooBarBasicAuthHeader))
+    ) {
+      body shouldBe s"""[{"fileName":"a.txt","dirPath":"files/a.txt","sha1sum":"x"}]"""
+      status shouldBe OK_200
     }
   }
 }
