f14ead84254b9fcb480b92a8383860bdb6393b01
alexal14 <alexander.atamas@dans.knaw.nl>
2018-04-06 16:36:50 +0200

    Deposit (#21)
    * create deposit plus unit test
    
    * Lists the deposits of the specified user
    
    * filter isDirectory was added
    
    * a small fix for user:String
    
    * create deposit and list all diposits with unit tests
    
    * EASY-1491,1492
    
    * get method is added
    
    * names of variables are changed

--- src/test/scala/nl.knaw.dans.easy.deposit/DepositDirSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.deposit/DepositDirSpec.scala
@@ -19,12 +19,14 @@ import java.nio.file.attribute.PosixFilePermission
 import scala.util.{ Failure, Success}
 
 class DepositDirSpec extends TestSupportFixture {
-  before { clearTestDir() }
+  before {
+    clearTestDir()
+    draftsDir.createDirectories()
+  }
   private val draftsDir = testDir / "drafts"
 
   "DepositDir.create" should "fail if the dir 'draft' is read only" in {
-
-    draftsDir.createDirectories()
+    draftsDir
       .removePermission(PosixFilePermission.OWNER_WRITE)
     inside(DepositDir.create(draftsDir, "user001")) {
       case Failure(_) =>
@@ -32,9 +34,9 @@ class DepositDirSpec extends TestSupportFixture {
   }
 
   it should "create a new directory with deposit.properties" in {
-    val dd = DepositDir.create(draftsDir, "user001")
-    dd shouldBe a[Success[_]]
-    inside(dd) {
+    val tryDeposit = DepositDir.create(draftsDir, "user001")
+    tryDeposit shouldBe a[Success[_]]
+    inside(tryDeposit) {
       case Success(d) =>
         val dir = draftsDir / "user001" / d.id.toString
         dir.toJava should exist
@@ -50,25 +52,38 @@ class DepositDirSpec extends TestSupportFixture {
   }
 
   "list" should """show no deposits of "user001" user""" in {
-    draftsDir.createDirectories()
-    inside(DepositDir.list(draftsDir, "user001")) {
-      case Success(Seq()) =>
+    val tryDeposits = DepositDir.list(draftsDir, "user001")
+    tryDeposits shouldBe a[Success[_]]
+    inside(tryDeposits) {
+      case Success(list) => list shouldBe empty
     }
   }
 
   it should """show one deposit of "user001" user""" in {
-    draftsDir.createDirectories()
     DepositDir.create(draftsDir, "user001")
-    inside(DepositDir.list(draftsDir, "user001")) {
-      case Success(Seq(_)) =>
+    val tryDeposits = DepositDir.list(draftsDir, "user001")
+    tryDeposits shouldBe a[Success[_]]
+    inside(tryDeposits) {
+      case Success(list) => list should have length 1
     }
   }
 
   it should """show more than two deposits of "user001" user""" in {
-    draftsDir.createDirectories()
     for (i <- 1 to 3) DepositDir.create(draftsDir, "user001")
-    inside(DepositDir.list(draftsDir, "user001")) {
-      case Success(Seq(_,_,_)) =>
+    val tryDeposits = DepositDir.list(draftsDir, "user001")
+    tryDeposits shouldBe a[Success[_]]
+    inside(tryDeposits) {
+      case Success(list) => list should have length 3
     }
   }
+
+  "get" should """return a specified deposit""" in {
+    val deposit = DepositDir.create(draftsDir, "user001").get
+    val tryDeposit = DepositDir.get(draftsDir, "user001", deposit.id)
+    tryDeposit shouldBe a[Success[_]]
+    inside(tryDeposit) {
+      case Success(dp) => dp shouldBe deposit
+    }
+  }
+
 }
