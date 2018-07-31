34434fea08323f29202140f39ba810e5023e3c3a
Joke Pol <joke.pol@dans.knaw.nl>
2018-07-31 15:10:19 +0200

    no changes with an invalid state transiction to submitted

--- src/test/scala/nl.knaw.dans.easy.deposit/servlets/IntegrationSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.deposit/servlets/IntegrationSpec.scala
@@ -184,6 +184,7 @@ class IntegrationSpec extends TestSupportFixture with ServletFixture with Scalat
       status shouldBe FORBIDDEN_403
       body shouldBe s"Cannot transition from ARCHIVED to SUBMITTED (deposit id: $uuid, user: foo)"
     }
+    (testDir / "drafts" / "foo" / uuid.toString / "metatada").entries.size shouldBe 1 // still just the json
   }
 
   private def randomContent(times: Int) = {
