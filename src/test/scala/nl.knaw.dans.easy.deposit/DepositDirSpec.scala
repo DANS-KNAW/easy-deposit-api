1835ba7ec1a39c9ca1be35900688e387ff50457a
Stephan Hogenboom <stephan.hogenboom@dans.knaw.nl>
2018-11-06 17:08:41 +0100

    adjusted unit test according to change

--- src/test/scala/nl.knaw.dans.easy.deposit/DepositDirSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.deposit/DepositDirSpec.scala
@@ -140,7 +140,8 @@ class DepositDirSpec extends TestSupportFixture with MockFactory {
 
     // preconditions
     mdFile.contentAsString shouldBe "{}"
-    oldDepositProperties should not include "identifier.doi"
+    oldDepositProperties should include("identifier.doi.registered = no")
+    oldDepositProperties should not include "identifier.doi = "
     val pidMocker = mock[PidRequester]
     (pidMocker.requestPid(_: PidType)) expects * once() returning Success(doi)
 
