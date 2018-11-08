22f208c9a8f9088dee3f4f374ab88545e1e1660f
Joke Pol <joke.pol@dans.knaw.nl>
2018-11-08 10:27:30 +0100

    scala doc

--- src/main/scala/nl.knaw.dans.easy.deposit/logging/CookieMasker.scala
+++ src/main/scala/nl.knaw.dans.easy.deposit/logging/CookieMasker.scala
@@ -2,6 +2,7 @@ package nl.knaw.dans.easy.deposit.logging
 
 trait CookieMasker {
 
+  /** The default implementation keeps the name of the cookie, masks everything of the value but dots. */
   protected def maskCookieHeader(value: String): String = {
     val cookieName = value.replaceAll("=.*", "")
     val cookieValue = value.replaceAll(".*=", "")
