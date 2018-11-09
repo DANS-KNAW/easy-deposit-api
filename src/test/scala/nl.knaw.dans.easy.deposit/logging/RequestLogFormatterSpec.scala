e52b7dc7e0c24930ffa706b8543f4a9ff593ae40
Joke Pol <joke.pol@dans.knaw.nl>
2018-11-09 10:22:36 +0100

    simplified test servlet

--- src/test/scala/nl.knaw.dans.easy.deposit/logging/RequestLogFormatterSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.deposit/logging/RequestLogFormatterSpec.scala
@@ -24,85 +24,79 @@ import org.scalatra.{ MultiParams, ScalatraServlet }
 
 class RequestLogFormatterSpec extends TestSupportFixture with MockFactory {
 
-  class DefaultTestServlet(params: MultiParams) extends ScalatraServlet with RequestLogFormatter {
+  class DefaultTestServlet(params: MultiParams = mockParams,
+                           override val request: HttpServletRequest = mockRequest
+                          ) extends ScalatraServlet with RequestLogFormatter {
 
     override def multiParams(implicit request: HttpServletRequest): MultiParams = params
 
-    def log(request: HttpServletRequest): String = formatRequestLog(request)
+    def log: String = formatRequestLog(request)
   }
 
   "formatRequestLog" should "mask everything" in {
-    new DefaultTestServlet(mockParams).log(mockRequest) shouldBe
+    new DefaultTestServlet().log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.**.**; params=[password -> *****, login -> *****]; headers=[cookie -> scentry.auth.default.user=******.**.**, HTTP_AUTHORIZATION -> basic *****, foo -> bar]"
   }
 
   it should "mask nothing" in {
 
-    case class TestServlet() extends
-      DefaultTestServlet(mockParams) with PlainRemoteAddress with PlainParameters with PlainHeaders
-    TestServlet().log(mockRequest) shouldBe
+    new DefaultTestServlet() with PlainRemoteAddress with PlainParameters with PlainHeaders {}.log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.56.78; params=[password -> secret, login -> mystery]; headers=[cookie -> scentry.auth.default.user=abc456.pq.xy, HTTP_AUTHORIZATION -> basic 123x_, foo -> bar]"
   }
 
   it should "mask nothing but cookie headers" in {
 
-    case class TestServlet() extends
-      DefaultTestServlet(mockParams) with PlainRemoteAddress with PlainParameters with PlainAuthorizationHeader
-    TestServlet().log(mockRequest) shouldBe
+    new DefaultTestServlet() with PlainRemoteAddress with PlainParameters with PlainAuthorizationHeader {}.log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.56.78; params=[password -> secret, login -> mystery]; headers=[cookie -> scentry.auth.default.user=******.**.**, HTTP_AUTHORIZATION -> basic 123x_, foo -> bar]"
   }
 
   it should "add a custom message to the log line" in {
 
-    case class TestServlet() extends DefaultTestServlet(mockParams) {
+    new DefaultTestServlet {
       override protected def formatRequestLog(implicit request: HttpServletRequest): String = {
         super.formatRequestLog(request) + " custom message"
       }
-    }
-    TestServlet().log(mockRequest) shouldBe
+    }.log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.**.**; params=[password -> *****, login -> *****]; headers=[cookie -> scentry.auth.default.user=******.**.**, HTTP_AUTHORIZATION -> basic *****, foo -> bar] custom message"
   }
 
   it should "mask all headers in the same way" in {
 
-    case class TestServlet() extends DefaultTestServlet(mockParams) {
+    new DefaultTestServlet {
       override protected def formatHeaders(headers: Map[String, String]): Map[String, String] = {
         headers.map { case (key: String, _: String) => (key, "*") }
       }
-    }
-    TestServlet().log(mockRequest) shouldBe
+    }.log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.**.**; params=[password -> *****, login -> *****]; headers=[cookie -> *, HTTP_AUTHORIZATION -> *, foo -> *]"
   }
 
   it should "mask all headers, even their names" in {
 
-    case class TestServlet() extends DefaultTestServlet(mockParams) {
+    new DefaultTestServlet {
       override protected def headersToString(headers: Map[String, String]): String = "*****"
     }
-    TestServlet().log(mockRequest) shouldBe
+      .log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.**.**; params=[password -> *****, login -> *****]; headers=*****"
   }
 
   it should "mask also a (case sensitive) custom header" in {
 
-    case class TestServlet() extends DefaultTestServlet(mockParams) {
+    new DefaultTestServlet {
       override protected def formatHeaders(headers: Map[String, String]): Map[String, String] = {
         super.formatHeaders(headers).map {
           case ("foo", _: String) => ("foo", "***")
           case kv => kv
         }
       }
-    }
-    TestServlet().log(mockRequest) shouldBe
+    }.log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.**.**; params=[password -> *****, login -> *****]; headers=[cookie -> scentry.auth.default.user=******.**.**, HTTP_AUTHORIZATION -> basic *****, foo -> ***]"
   }
 
   it should "log the number of parameters (alias named fields in web forms), not their names/values" in {
 
-    case class TestServlet() extends DefaultTestServlet(mockParams) {
+    new DefaultTestServlet {
       override protected def parametersToString(params: Map[String, String]): String = params.size.toString
-    }
-    TestServlet().log(mockRequest) shouldBe
+    }.log shouldBe
       "GET http://does.not.exist.dans.knaw.nl remote=12.34.**.**; params=2; headers=[cookie -> scentry.auth.default.user=******.**.**, HTTP_AUTHORIZATION -> basic *****, foo -> bar]"
   }
 
