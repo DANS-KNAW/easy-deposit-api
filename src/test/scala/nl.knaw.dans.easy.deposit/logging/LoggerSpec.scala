1f272701ad94801d5d6f04eaab928b864a1b62b0
Jo Pol <jo-pol@users.noreply.github.com>
2018-11-15 11:31:28 +0100

    license header

--- src/test/scala/nl.knaw.dans.easy.deposit/logging/LoggerSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.deposit/logging/LoggerSpec.scala
@@ -1,4 +1,19 @@
 /**
+ * Copyright (C) 2018 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * value1 2018 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -64,7 +79,8 @@ class LoggerSpec extends TestSupportFixture with ServletFixture with ScalatraSui
 
   trait CustomRequestLogFormatter extends RequestLogFormatter with PlainRemoteAddress {
     this: ScalatraBase =>
-    // or other overrides as documented in XxxLogFormatterSpec
+    // or other overrides (explicit and through traits)
+    // are documented in XxxLogFormatterSpec
   }
 
   "separate custom loggers" should "override default loggers" in {
