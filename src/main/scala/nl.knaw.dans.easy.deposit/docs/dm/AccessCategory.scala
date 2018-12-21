131a256ad7fb2fca22b903fd1f1c208a4d259498
Jo Pol <jo-pol@users.noreply.github.com>
2018-12-21 09:47:59 +0100

    reduced access category values to those in UI

--- src/main/scala/nl.knaw.dans.easy.deposit/docs/dm/AccessCategory.scala
+++ src/main/scala/nl.knaw.dans.easy.deposit/docs/dm/AccessCategory.scala
@@ -20,11 +20,7 @@ import nl.knaw.dans.easy.deposit.docs.dm.AccessCategory.AccessCategory
 object AccessCategory extends Enumeration {
   type AccessCategory = Value
   val open: AccessCategory = Value("OPEN_ACCESS")
-  val openForRegisteredUsers: AccessCategory = Value("OPEN_ACCESS_FOR_REGISTERED_USERS")
-  val restrictedGroup: AccessCategory = Value("GROUP_ACCESS")
   val restrictedRequest: AccessCategory = Value("REQUEST_PERMISSION")
-  val otherAccess: AccessCategory = Value("NO_ACCESS")
-  // TODO drop some as in https://github.com/DANS-KNAW/easy-deposit-ui/pull/81
 }
 
 case class AccessRights(category: AccessCategory,
