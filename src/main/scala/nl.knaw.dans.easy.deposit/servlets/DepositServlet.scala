c1edc81d60adb418a66be0661db7f17cc151bb82
Jo Pol <jo-pol@users.noreply.github.com>
2019-05-06 12:17:44 +0200

    rename requeasted by review comment

--- src/main/scala/nl.knaw.dans.easy.deposit/servlets/DepositServlet.scala
+++ src/main/scala/nl.knaw.dans.easy.deposit/servlets/DepositServlet.scala
@@ -152,10 +152,10 @@ class DepositServlet(app: EasyDepositApiApp)
         path <- getPath
         _ <- isMultipart
         fileItems = fileMultiParams.valuesIterator.flatten.buffered
-        maybeZipInputStream <- fileItems.nextAsZipIfOnlyOne
+        maybeManagedZipInputStream <- fileItems.nextAsZipIfOnlyOne
         (managedStagingDir, stagedFilesTarget) <- app.stageFiles(user.id, uuid, path)
         _ <- managedStagingDir.apply(stagingDir =>
-          maybeZipInputStream
+          maybeManagedZipInputStream
             .map(_.unzipPlainEntriesTo(stagingDir))
             .getOrElse(app.multipartConfig.moveNonZips(fileItems, stagingDir))
             .flatMap(_ => stagedFilesTarget.moveAllFrom(stagingDir))
